793be536f0844e6862e778d4bdee538acf788838
libinbin000 <1132411067@qq.com>
2018-11-01 14:11:38 +0800

    first

---
+++ TexturePackerSplit_json/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexturePackerSplit_json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = null;
+
+            if (args.Length <= 0)
+            {
+                path = Console.ReadLine();
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-p":
+                            if (args.Length - 1 < i + 1)
+                            {
+                                Console.WriteLine("参数不足");
+                                return;
+                            }
+                            path = args[i + 1];
+                            i++;
+                            break;
+                        default:
+                            Console.WriteLine("命令:" + args[i] + "不存在");
+                            break;
+                    }
+                }
+            }
+
+            
+
+            if (path == null)
+            {
+                Console.WriteLine("参数不足");
+                return;
+            }
+
+            TexturePackerSplit_json tool = new TexturePackerSplit_json();
+            string error = "";
+            if (tool.init(path, out error) == false) {
+                Console.WriteLine("[TexturePackerSplit]"+ error);
+                return;
+            }
+
+        }
+    }
+}
---
+++ TexturePackerSplit_json/TexturePackerSplit_json.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexturePackerSplit_json
+{
+    public class TexturePackerSplit_json
+    {
+        private string jsonPath;
+        private string pngPath;
+        private string savePath;
+        private Bitmap png;
+
+        public TexturePackerSplit_json()
+        {
+            
+        }
+
+        public bool init(string path,out string err) {
+            err = "";
+            jsonPath = path;
+            
+            
+            //判断文件是否存在
+            if (File.Exists(path) == false)
+            {
+                err = "文件不存在";
+                return false;
+            }
+
+            //判断文件是否为json
+            if (Path.GetExtension(path) != ".json")
+            {
+                err = "图集数据必须为json";
+                return false;
+            }
+
+            //判断Png是否存在
+            pngPath = Path.ChangeExtension(path, "png");
+            if(File.Exists(pngPath)==false)
+            {
+                err = "同名png不存在";
+                return false;
+            }
+
+            //加载png
+            png = new Bitmap(pngPath);
+            if (png == null)
+            {
+                err = "创建png失败";
+                return false;
+            }
+
+            //创建目录
+            savePath = Path.GetDirectoryName(path)+"/"+Path.GetFileNameWithoutExtension(path);
+            if (Directory.Exists(savePath)==false)
+            {
+                Directory.CreateDirectory(savePath);
+            }
+
+            //解析json
+            StreamReader sr = new StreamReader(jsonPath, Encoding.Default);
+            string content;
+            string str_json="";
+            while ((content = sr.ReadLine()) != null)
+            {
+                str_json += content.ToString()+"\n";
+            }
+
+            Console.WriteLine("[TexturePackerSplit]生成目录->" + savePath);
+            //遍历子图像数据并切图
+            JObject jObject = (JObject)JsonConvert.DeserializeObject(str_json);
+            foreach (JProperty v in jObject["frames"])
+            {
+                //获取子图像数据
+                int x = Convert.ToInt32(v.Value["frame"]["x"]);
+                int y = Convert.ToInt32(v.Value["frame"]["y"]);
+                int w = Convert.ToInt32(v.Value["frame"]["w"]);
+                int h = Convert.ToInt32(v.Value["frame"]["h"]);
+                Rectangle range = new Rectangle(x,y,w,h);
+
+                //克隆子图像
+                Bitmap child = img_tailor(png, range);
+                string saveFullName = savePath + "/" + v.Name;
+                img2file(child, saveFullName);
+
+                Console.WriteLine("[TexturePackerSplit]生成图像->" + v.Name);
+            }
+
+
+            return true;
+        }
+
+        public static Bitmap img_tailor(Bitmap src, Rectangle range)
+        {
+             return src.Clone(range, System.Drawing.Imaging.PixelFormat.DontCare);
+        }
+        
+        //图片生成
+        public static void img2file(Bitmap b, string filepath)
+        {
+            b.Save(filepath);
+        }
+    }
+}
